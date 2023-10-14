7b412939e18c830aca6a1900984950bc463852b9
Guilherme S <guinho.shuto@gmail.com>
2023-10-14 19:17:14 -0400

    oi

---
+++ src/test/java/br/com/guinhoshuto/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guinhoshuto.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
